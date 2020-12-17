c1abf90b5c2952595d780c2fc4226a44a4838f22
ritchey0713 <ritchey0713@gmail.com>
2020-12-17 18:22:09 -0500

    commit tree 2ddf500cdc1fb3f3d295eaebb2ca53c3d6f6d74f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
-def oxford_comma(array)
+# 2 strings will have an "and"
+# > 2 string will have commas and a "and"
 
+
+def oxford_comma(array)
+  # if array.length == 2 
+  #   "#{array.first} and #{array.last}"
+  # elsif array.length > 2
+  #   # array.join(", ").insert(-3, "and ")
+  #   # array.insert(-2, "and").join(", ")
+  #   # something = array.pop
+  #   # array << "and #{something}"
+  #   # array.join(", ")
+  #   array[-1].insert(0, "and ")
+  # else 
+  #   array.first
+  # end 
+  # array.join(", ")
+
+  if array.length == 2 
+    "#{array.first} and #{array.last}"
+  elsif array.length > 2 
+    array[-1].insert(0, "and ")
+  end 
+  array.join(", ")
 end
